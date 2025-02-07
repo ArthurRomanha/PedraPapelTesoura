4561940febbe2fb30cdcfbb5571fafa7c537510f
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-02-07 12:35:00 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock - Paper - Scissors
+Credits
+<a href="https://www.flaticon.com/br/icones-gratis/do-utilizador" title="do utilizador ícones">Do utilizador ícones criados por Freepik - Flaticon</a>
--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 let escolhaRobo;
-let minhaEscolha;
-const escolha = (num) => {
-    switch (num) {
-        case 0:
-            minhaEscolha = "pedra";
-            break;
-        case 1:
-            minhaEscolha = "papel";
-            break;
-        case 2:
-            minhaEscolha = "tesoura";
-            break;
-    }
+const minhaEscolha = () => {
+    let myChoice = document.querySelector('input[name="elemento"]:checked');
+    console.log(myChoice.value);
+    
 }
 const sorteiaEscolhaRobo = () => {
     let num = Math.floor(Math.random()*3);
+    console.log(num);
+    
     switch (num) {
         case 0:
             minhaEscolha = "pedra";
@@ -30,10 +23,15 @@ const sorteiaEscolhaRobo = () => {
 const verificaVitória = () => {
     let ganhou;
     if((minhaEscolha == "papel" && escolhaRobo == "pedra")||(minhaEscolha == "tesoura" && escolhaRobo == "papel")||(minhaEscolha == "pedra" && escolhaRobo == "tesoura")){
-
+        ganhou=true;
+        return ganhou;
+    }else{
+        ganhou=false;
+        return ganhou;
     }
 }
 const start = () => {
+    minhaEscolha()
     sorteiaEscolhaRobo();
     verificaVitória();
 }
